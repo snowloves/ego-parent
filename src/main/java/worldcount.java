4396ef2a8e0bede846bb673e6050839437d86d4d
zhangjunhua <15565322011@163.com>
2019-03-17 17:31:53 +0800

    解决冲突

--- src/main/java/worldcount.java
+++ src/main/java/worldcount.java
@@ -1,5 +1,5 @@
 public class worldcount {
     public static void main(String[] args) {
-        System.out.println("worldcount python java scala  flume");
+        System.out.println("worldcount python java scala  java ");
     }
 }
